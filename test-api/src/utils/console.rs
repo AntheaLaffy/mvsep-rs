799ca526b85b3f3e505a76288764b622d8ea102f
Kisaragi Fuurin <NicoleSandwich@outlook.com>
2026-07-09 17:19:44 +0800

    Fix Windows cross-compilation for test API

--- test-api/src/utils/console.rs
+++ test-api/src/utils/console.rs
@@ -2,19 +2,8 @@ pub fn init() {
     #[cfg(windows)]
     {
         if let Err(e) = colored::control::set_virtual_terminal(true) {
-            eprintln!("Warning: Failed to enable virtual terminal: {}", e);
+            eprintln!("Warning: Failed to enable virtual terminal: {:?}", e);
         }
-    }
-
-    #[cfg(windows)]
-    unsafe {
-        use std::sync::Once;
-        static INIT: Once = Once::new();
-        INIT.call_once(|| {
-            if let Some(lib) = winapi::shared::minwindef::HMODULE::from_raw(1) {
-                let _ = lib;
-            }
-        });
     }
 
     #[cfg(not(windows))]
